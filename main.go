a01727b746e2b3c2792bc81f23ad0ed9d95a1ef9
JanainaLudwig <janainaludwig@gmail.com>
2021-04-25 21:30:56 -0300

    Format

--- main.go
+++ main.go
@@ -3,25 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
-	post2 "golang-proxy/model/post"
+	"golang-proxy/model/post"
 )
 
 func main()  {
 	bg := context.Background()
-	ctx := context.WithValue(bg, "auth_id", 1)
+	p := post.NewPostRepository()
 
-	p := post2.NewPostRepository()
+	// For user 1
+	ctx := context.WithValue(bg, "auth_id", 1)
 
 	posts, err := p.GetAll(ctx)
 	fmt.Println("[All posts - USER 1]", posts, err)
 
-	post, err := p.GetById(ctx, 1)
-	fmt.Println("[Post ID 1 - USER 1]", post, err)
+	post1, err := p.GetById(ctx, 1)
+	fmt.Println("[Post ID 1 - USER 1]", post1, err)
 
 	post3, err := p.GetById(ctx, 3)
 	fmt.Println("[Post ID 3 - USER 1]", post3, err)
 
+
+	// For user 2
 	ctx2 := context.WithValue(bg, "auth_id", 2)
+
+	posts, err = p.GetAll(ctx2)
+	fmt.Println("[All posts - USER 1]", posts, err)
 
 	post3, err = p.GetById(ctx2, 3)
 	fmt.Println("[Post ID 3 - USER 2]", post3, err)
